f06161c36abbe97a1a763090825c030c33e55f9e
takewiki <hulilei@takeiwki.com.cn>
2023-03-29 13:49:03 +0000

    v1.5

--- R/accountcodepm.R
+++ R/accountcodepm.R
@@ -6,6 +6,7 @@
 #' @export
 #'
 #' rds_accountcode_pm(token = "C0426D23-1927-4314-8736-A74B2EF7A039")
+
 rds_accountcode_pm <- function(token = "C0426D23-1927-4314-8736-A74B2EF7A039") {
 
   sql = "UPDATE A SET A.F_NLJ_ACCOUNTCODE1 = B.F_NLJ_ACCOUNTCODE

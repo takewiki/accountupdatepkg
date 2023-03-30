8dc525e17da6e640594138930497f29a60297189
takewiki <hulilei@takeiwki.com.cn>
2023-03-30 01:53:52 +0000

    V1.7

--- R/accountcodepo.R
+++ R/accountcodepo.R
@@ -9,26 +9,26 @@
 #'
 rds_accountcode_po <- function(token = "C0426D23-1927-4314-8736-A74B2EF7A039") {
 
-  sql = "	update A  SET A.F_NLJ_ACCOUNTCODE = D.FNUMBER
-        	FROM
-        	t_PUR_POOrderEntry A
-        	INNER JOIN
-        	T_BD_ACCOUNT_L C
-        	ON C.FACCTID = A.F_NLJ_ACCOUNTCODE
-        	inner join
-        	T_BD_ACCOUNT D
-        	on C.FACCTID = D.FACCTID
-        	inner join
-        	T_PUR_POORDERINSTALLMENT B
-        	on A.FID =B.FID
-        	AND A.FSEQ = B.FSEQ
-        	INNER JOIN
-        	t_PUR_POOrder  E
-        	ON E.FID =B.FID
-        	 inner join   t_BD_Supplier  F
-        	on E.FSUPPLIERID = F.FSUPPLIERID
-        	WHERE  F.F_NLJ_CHECKBOX = 1
-        	AND  A.F_NLJ_ACCOUNTCODE <> ' '
+  sql = "			 update A  SET A.F_NLJ_ACCOUNTCODE1 = D.FNUMBER
+            	FROM
+            	T_PUR_POORDERINSTALLMENT A
+            	INNER JOIN
+            	t_PUR_POOrderEntry  B
+            	on A.FID =B.FID
+            	AND A.FSEQ = B.FSEQ
+            	inner join
+            	T_BD_ACCOUNT_L C
+            	ON C.FACCTID = B.F_NLJ_ACCOUNTCODE
+            	inner join
+            	T_BD_ACCOUNT D
+            	on C.FACCTID = D.FACCTID
+            	INNER JOIN
+            	t_PUR_POOrder  E
+            	ON E.FID =B.FID
+            	 inner join   t_BD_Supplier  F
+            	on E.FSUPPLIERID = F.FSUPPLIERID
+            	WHERE  F.F_NLJ_CHECKBOX = 1
+            	AND  B.F_NLJ_ACCOUNTCODE <> ' '
        "
   res = tsda::sql_update2(token = token,sql_str = sql)
   return(res)
